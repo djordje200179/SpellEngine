5921160b9591c5f4604d7c55804043d0507e16a2
Đorđe Milanović <djordje200179@gmail.com>
2021-10-04 23:07:24 +0200

    Files renamed

--- SpellEngine/pch.hpp
+++ SpellEngine/pch.hpp
@@ -3,4 +3,4 @@
 #define WIN32_LEAN_AND_MEAN  
 
 #include "SpellEngine/node.hpp"
-#include "SpellEngine/spell_engine.hpp"
+#include "SpellEngine/spell-engine.hpp"
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <SpellEngine/spell_engine.hpp>
+#include <SpellEngine/spell-engine.hpp>
 
 void test(const SpellEngine::SpellEngine& spellEngine, const std::string& word) {
 	auto result = spellEngine(word);
@@ -11,7 +11,7 @@ void test(const SpellEngine::SpellEngine& spellEngine, const std::string& word) 
 int main() {
 	auto spellEngine = SpellEngine::SpellEngine();
 
-	auto inputFile = std::ifstream("words_alpha.txt");
+	auto inputFile = std::ifstream("words-alpha.txt");
 	inputFile >> spellEngine;
 	inputFile.close();
 
